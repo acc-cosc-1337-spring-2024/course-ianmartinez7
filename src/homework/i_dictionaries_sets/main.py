4d3a5db5d65c2d8663768e969d32bdd56e708e40
ianmartinez7 <157126536+ianmartinez7@users.noreply.github.com>
2024-04-07 05:19:41 +0000

    working menu in main, with commented our menu from homework 6 and testing in sets.py

--- src/homework/i_dictionaries_sets/main.py
+++ src/homework/i_dictionaries_sets/main.py
@@ -1,6 +1,7 @@
 #
 
 from dictionary import add_inventory, remove_inventory_widget
+from sets import get_students_who_took_prog1_and_prog2, get_students_who_took_prog1_not_prog2, get_students_who_took_prog1_or_prog2, get_students_who_took_prog2_not_prog1
 
 def main_menu():
     inventory = {}
@@ -32,5 +33,44 @@ def main_menu():
             break
         else:
             print("Invalid option, please try again.")
+#commenting out for working homework 7 menu
+#main_menu()
 
-main_menu()
+
+def menu_homework_7():
+
+    prog1 = {'Student1', 'Student2', 'Student3'}
+    prog2 = {'Student3', 'Student4', 'Student5'}
+
+    while True:
+        print("\nInventory Menu")
+        print("1-Students who took prog1 and prog2")
+        print("2-Students who took prog2 only")
+        print("3-Students who took prog1 not prog2")
+        print("4-Students who took prog2 not prog1")
+        print("5-Exit")
+        print("")
+
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            print(" ")
+            print(get_students_who_took_prog1_and_prog2(prog1, prog2))
+        elif choice == '2':
+            print(" ")
+            print(get_students_who_took_prog1_or_prog2(prog1, prog2))
+        elif choice == '3':
+            print(" ")
+            print(get_students_who_took_prog1_not_prog2(prog1, prog2))
+        elif choice == '4':
+            print(" ")
+            print(get_students_who_took_prog2_not_prog1(prog1, prog2))
+        elif choice == '5':
+            print(" ")
+            print("Exiting the program.")
+            break
+        else:
+            print(" ")
+            print("Invalid choice. Please try again.")
+
+menu_homework_7()
--- src/homework/i_dictionaries_sets/sets.py
+++ src/homework/i_dictionaries_sets/sets.py
@@ -17,17 +17,17 @@ def get_students_who_took_prog2_not_prog1(prog1, prog2):
     return prog2.difference(prog1)
 
 
-prog1 = {'Student1', 'Student2', 'Student3'}
-prog2 = {'Student3', 'Student4', 'Student5'}
-
-
-students_who_took_both = get_students_who_took_prog1_and_prog2(prog1, prog2)
-students_who_took_either = get_students_who_took_prog1_or_prog2(prog1, prog2)
-students_who_took_prog1_only = get_students_who_took_prog1_not_prog2(prog1, prog2)
-students_who_took_prog2_only = get_students_who_took_prog2_not_prog1(prog1, prog2)
+#prog1 = {'Student1', 'Student2', 'Student3'}
+#prog2 = {'Student3', 'Student4', 'Student5'}
 
 #for testing purposes
-print('Students who took both programs:', students_who_took_both)
-print('Students who took either program:', students_who_took_either)
-print('Students who took only the first program:', students_who_took_prog1_only)
-print('Students who took only the second program:', students_who_took_prog2_only)
+#students_who_took_both = get_students_who_took_prog1_and_prog2(prog1, prog2)
+#students_who_took_either = get_students_who_took_prog1_or_prog2(prog1, prog2)
+#students_who_took_prog1_only = get_students_who_took_prog1_not_prog2(prog1, prog2)
+#students_who_took_prog2_only = get_students_who_took_prog2_not_prog1(prog1, prog2)
+
+#for testing purposes
+#print('Students who took both programs:', students_who_took_both)
+#print('Students who took either program:', students_who_took_either)
+#print('Students who took only the first program:', students_who_took_prog1_only)
+#print('Students who took only the second program:', students_who_took_prog2_only)
