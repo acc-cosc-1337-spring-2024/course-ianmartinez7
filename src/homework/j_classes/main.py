638f91fe336ec00fe7aea1c1801eeacf051ab8f3
ianmartinez7 <157126536+ianmartinez7@users.noreply.github.com>
2024-04-21 05:27:42 +0000

    working menu with class code

--- src/homework/j_classes/main.py
+++ src/homework/j_classes/main.py
@@ -2,7 +2,7 @@
 
 from class_a import Die
 
-#### test case to make sure code works
+''' can ignore this now was just testing the class
 my_die = Die()
 my_die.roll()
 print(my_die)
@@ -10,4 +10,21 @@ my_die.roll()
 print("New roll:", my_die.get_rolled_value())
 my_die.roll()
 print("Another roll:", my_die)
-### end of the test case
+ '''
+
+
+def main():
+    die = Die()  
+    while True:
+        user_input = input("Roll the die? (yes/no): ")  
+        if user_input.lower() == "yes":
+            die.roll()  
+            print(die)  
+        elif user_input.lower() == "no":
+            print("Exiting the program.")
+            break  
+        else:
+            print("Invalid input. Please enter 'yes' or 'no'.")
+
+main()
+
