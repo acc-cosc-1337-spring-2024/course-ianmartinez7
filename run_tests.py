b8326c59d51e9e68825c97ec8933ca40a53fc1a2
ianmartinez7 <157126536+ianmartinez7@users.noreply.github.com>
2024-04-21 05:19:52 +0000

    working code for test case, all passed

--- run_tests.py
+++ run_tests.py
@@ -1,12 +1,8 @@
-import unittest
-'''
-the file in /tests/homework/b_in_proc_out/tests_in_proc_out
-has the test functions
-'''
-from tests.homework.i_dictionaries_sets import tests_dictionaries_and_sets
+import unittest  # Import unittest for testing, use this for each test case run
+from tests.homework.j_classes import tests_classes
 
+suite = unittest.TestLoader().loadTestsFromModule(tests_classes)
 
-suite = unittest.TestLoader().loadTestsFromModule(tests_dictionaries_and_sets)
 unittest.TextTestRunner(verbosity=2).run(suite)
 
 
--- tests/homework/j_classes/tests_classes.py
+++ tests/homework/j_classes/tests_classes.py
@@ -2,7 +2,26 @@
 import unittest
 from src.homework.j_classes.class_a import Die
 
+class TestDie(unittest.TestCase):
+    def setUp(self):
+        self.die = Die()
 
-def test_Die(self):
+    def test_roll(self):
+        self.die.roll() 
+        roll_value = self.die.get_rolled_value()  # Get the rolled value
+        self.assertIn(roll_value, [1, 2, 3, 4, 5, 6], "Roll should be between 1 and 6")
 
-    
+    def test_str(self):
+        self.die.roll()  # Roll the die
+        expected_str = f"The rolled value is {self.die.get_rolled_value()}"
+        self.assertEqual(str(self.die), expected_str, "String representation should be accurate")
+
+    def test_repeated_rolls(self):
+        # Roll several times to ensure different values
+        roll_values = []
+        for _ in range(10):
+            self.die.roll()
+            roll_values.append(self.die.get_rolled_value())
+        self.assertTrue(len(set(roll_values)) > 1, "Repeated rolls should produce varying results")
+
+ 
